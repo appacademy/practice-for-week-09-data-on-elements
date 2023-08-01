42dd52fadd3b85288a621a494b77a6ae2fc333ac
vi xent <alagovincent03@gmailcom>
2023-08-01 14:57:49 -0700

    done

--- data.js
+++ data.js
@@ -1,1 +1,19 @@
-// Your code here
+// Your code hereadd
+document.addEventListener("DOMContentLoaded",event=>{
+
+    let itemName = document.getElementById('name')
+    let itemClass = document.getElementById('type')
+    let ul = document.getElementById('shopping-list')
+    let addButton = document.getElementById('add')
+
+    addButton.addEventListener("click",event =>{
+        event.preventDefault()
+
+        const newLi = document.createElement("li")
+        newLi.innerText = itemName.value
+        ul.appendChild(newLi)
+
+
+        newLi.setAttribute("data-type",itemClass.value)
+    })
+})
